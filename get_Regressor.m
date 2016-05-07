0ab86d97bda87801bbdc909dd3334b387048d4ee
gyy150 <gyy150@163.com>
2016-05-08 00:47:49 +0800

    getAllFiles treaverse through all the subdir of a given dir, treating each subdir as an calss, calculate the image regressor for each class and append it in the final output.
    get_image_vector  convert an given image to the image vector spercified
    in the paper

---
+++ get_Regressor.m
@@ -0,0 +1,7 @@
+function [ output_args ] = get_image_vector( image )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    
+
+end
+
---
+++ get_image_vector.m
@@ -0,0 +1,11 @@
+function image_vector = get_image_vector( image )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+im = imread(image);
+im = imresize(im,[10 5]);   %downsample the image 
+im = mat2gray(im);
+image_vector = im(:);
+
+
+end
+
