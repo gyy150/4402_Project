289c775033fd8078399793fdfe68d3e9cb67d94a
wizzfizz94 <21149116@student.uwa.edu.au>
2016-05-17 17:59:02 +0800

    gui + getClass

---
+++ cmpClass.m
@@ -0,0 +1,21 @@
+function [ dist , altPath ] = cmpClass( imVector, imagePath, classCell )
+
+    % get approx image vector
+    X = cell2mat(classCell(4));
+    H = X*inv(X'*X)*X';
+    approx_imVector = H*imVector;
+    
+    % calc distance
+    dist = sqrt(sum((approx_imVector - imVector).^2));
+    
+    % make sure output image isnt the same
+    trainingPaths = classCell(2);
+    b = 1;
+    altPath = trainingPaths{b};
+    while(strcmp(altPath,imagePath))
+        b = b + 1;
+        altPath = trainingPaths{b};
+    end
+
+end
+
