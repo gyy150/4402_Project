7915e39669961bccc2573eb2054bad8886754fd7
wizzfizz94 <21149116@student.uwa.edu.au>
2016-05-17 15:17:32 +0800

    start of gui

--- get_image_vector.m
+++ get_image_vector.m
@@ -2,7 +2,7 @@ function image_vector = get_image_vector( image )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 im = imread(image);
-im = imresize(im,[10 5]);   %downsample the image 
+im = imresize(im,[10 10]);   %downsample the image 
 im = mat2gray(im);
 image_vector = im(:);
 
