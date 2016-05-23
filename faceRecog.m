653b5d86d893e71256a8f8193d1021c7fb1b1311
20741091 <20741091@ecm-l-a-207-017.uniwa.uwa.edu.au>
2016-05-23 11:01:51 +0800

    updates to gui

--- faceRecog.m
+++ faceRecog.m
@@ -22,7 +22,7 @@ function varargout = faceRecog(varargin)
 
 % Edit the above text to modify the response to help faceRecog
 
-% Last Modified by GUIDE v2.5 20-May-2016 12:57:33
+% Last Modified by GUIDE v2.5 23-May-2016 09:05:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -98,28 +98,41 @@ handles.dirName  = uigetdir('.\*.*','Please select an image dataset directory to
 [~, onlyDir, ~] = fileparts(handles.dirName);
 
 % Retrieve the class size and image size
-[classSize, widthRes] = imageSetProp(handles.dirName);
-set(handles.maxSize,'String',int2str(widthRes));
+[handles.classSize, handles.widthRes] = imageSetProp(handles.dirName);
+set(handles.maxSize,'String',handles.widthRes);
+set(handles.sldrCurr,'String',get(handles.downSize, 'value')*handles.widthRes);
 
-if classSize > 0
+% Hide check boxes
+set(handles.checkbox1,'visible','off')
+set(handles.checkbox2,'visible','off') 
+set(handles.checkbox3,'visible','off') 
+set(handles.checkbox4,'visible','off')
+set(handles.checkbox5,'visible','off') 
+set(handles.checkbox6,'visible','off') 
+set(handles.checkbox7,'visible','off')
+set(handles.checkbox8,'visible','off') 
+set(handles.checkbox9,'visible','off') 
+set(handles.checkbox10,'visible','off')
+
+if handles.classSize > 0
     set(handles.checkbox1,'visible','on')
-    if classSize > 1
+    if handles.classSize > 1
         set(handles.checkbox2,'visible','on')
-        if classSize > 2
+        if handles.classSize > 2
             set(handles.checkbox3,'visible','on')
-            if classSize > 3
+            if handles.classSize > 3
                 set(handles.checkbox4,'visible','on')
-                if classSize > 4
+                if handles.classSize > 4
                     set(handles.checkbox5,'visible','on')
-                    if classSize > 5
+                    if handles.classSize > 5
                         set(handles.checkbox6,'visible','on')
-                        if classSize > 6
+                        if handles.classSize > 6
                             set(handles.checkbox7,'visible','on')
-                            if classSize > 7
+                            if handles.classSize > 7
                                 set(handles.checkbox8,'visible','on')
-                                if classSize > 8
+                                if handles.classSize > 8
                                     set(handles.checkbox9,'visible','on')
-                                    if classSize > 9
+                                    if handles.classSize > 9
                                         set(handles.checkbox10,'visible','on')
                                     end
                                 end
@@ -145,8 +158,10 @@ function downSize_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+set(handles.sldrCurr,'String',uint8(get(handles.downSize, 'value')*handles.widthRes));
+
+% Update handles structure
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -159,7 +174,6 @@ function downSize_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
-
 
 % --- Executes on button press in checkbox1.
 function checkbox1_Callback(hObject, eventdata, handles)
@@ -256,3 +270,60 @@ function simulate_Callback(hObject, eventdata, handles)
 % hObject    handle to simulate (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% Testing script for the project.
+
+% constants
+cb1 = get(handles.checkbox1, 'Value');
+cb2 = get(handles.checkbox2, 'Value');
+cb3 = get(handles.checkbox3, 'Value');
+cb4 = get(handles.checkbox4, 'Value');
+cb5 = get(handles.checkbox5, 'Value');
+cb6 = get(handles.checkbox6, 'Value');
+cb7 = get(handles.checkbox7, 'Value');
+cb8 = get(handles.checkbox8, 'Value');
+cb9 = get(handles.checkbox9, 'Value');
+cb10 = get(handles.checkbox10, 'Value');
+
+checkboxList = [cb1 cb2 cb3 cb4 cb5 cb6 cb7 cb8 cb9 cb10];
+selection = find(checkboxList);
+
+%numTrainingImages = 5;
+%scale = 50; 
+%selection = randperm(handles.classSize, numTrainingImages);
+
+% get test data                
+%dirName = uigetdir(pwd);                                    
+testData = getAllFiles(handles.dirName, selection , str2num(get(handles.sldrCurr, 'String')));   
+
+result = [];
+classes = {};
+numClasses = size(testData,1);
+
+% loop through classes in test data
+for i = 1:numClasses
+    counter = 0;
+    class_name =  testData{i,1};
+    
+    % get test paths
+    testPaths = testData{i,3};
+    numTestImages = size(testData{i,3},1);
+    
+    % for each test image calc class and check if correct 
+    for j = 1:numTestImages
+       file_path =  testPaths(j);
+       class = getClass(file_path, testData , str2num(get(handles.sldrCurr, 'String')));%result from getClass function.
+       detected_class_name = cell2mat(class(1));              %the class name as detected
+       distance = cell2mat(class(2));
+       if strcmp(detected_class_name,class_name );  %if correct
+           counter = counter +1;
+       end
+    end
+    result(i) = counter                      %put number of sucess of this class
+    classes{i} = class_name;
+end
+
+bar(result);
+Labels = class;
+set(gca, 'XTick', 1:4, 'XTickLabel', class);
+
