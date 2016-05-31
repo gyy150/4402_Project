45e6924891f0250b41c41a83bd9f10d37705c928
20741091 <20741091@ecm-l-a-207-022.uniwa.uwa.edu.au>
2016-05-31 16:30:30 +0800

    finshed commenting

--- imageSetProp.m
+++ imageSetProp.m
@@ -1,22 +1,32 @@
 function [classSize, heightRes, widthRes] = imageSetProp(dirName)
+% Read image dataset directory to determine class size and image dimensions
+% in order to populate GUI values
+%
+% Inputs:   dirName     - directory path with all images contained
+%
+% Output:   classSize   - number of images in class
+%           heightRes   - height of images
+%           widthRes    - width of images
+%
+% Authors:  Bradley Byrne, Zachary Newman, Yiyang Gao
 
-    dirData = dir(dirName);                             % Get the data for the current directory
-    dirIndex = [dirData.isdir];                         % Find the index for directories
-    fileList = {dirData(~dirIndex).name};               % Get a list of the files
-    subDirs = {dirData(dirIndex).name};                 % Get a list of the subdirectories
-    validIndex = ~ismember(subDirs,{'.','..'});         % Find index of subdirectories that are not '.' or '..'
-    
-    subDirList = find(validIndex);
-    subDir1 = fullfile(dirName,subDirs{subDirList(1)});
-    
-    subDirData = dir(subDir1);                             % Get the data for the current directory
-    subDirIndex = [subDirData.isdir];                      % Find the index for directories
-    subFileList = {subDirData(~subDirIndex).name};         % Get a list of the files
-    
-    classSize = length(subFileList);
-    
-    imString = fullfile(subDir1, subFileList(1));
-    testIm = imread(imString{1});
-    [heightRes, widthRes] = size(testIm);
+% Get the subdirectories for the current directory
+dirData = dir(dirName);
+dirIndex = [dirData.isdir];
+subDirs = {dirData(dirIndex).name};
+validIndex = ~ismember(subDirs,{'.','..'});
+
+% Read the first subdirectory and determine classSize
+subDirList = find(validIndex);
+subDir1 = fullfile(dirName,subDirs{subDirList(1)});
+subDirData = dir(subDir1);
+subDirIndex = [subDirData.isdir];              
+subFileList = {subDirData(~subDirIndex).name};
+classSize = length(subFileList);
+
+% Find first image resolution
+imString = fullfile(subDir1, subFileList(1));
+testIm = imread(imString{1});
+[heightRes, widthRes] = size(testIm);
 
 end
